c29c084639caac995f606e4176c75ea09c4766d2
Fabian Magrini <fabian.magrini@gmail.com>
2019-09-22 18:51:04 +1000

    tidied up environment scripts

--- SuperFundEnvironment/setup-tablestorage.sh
+++ SuperFundEnvironment/setup-tablestorage.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # Overide settings on the command line
-# ARG1 is resourceGroupName
+# ARG1 is resourceGroupName (optional)
+# ARG2 is environmentID (optional) must have resourceGroupName
 if [ $# -eq 0 ]
 then
   environmentID=$RANDOM
   resourceGroupName=superfund-rg-ts-$environmentID
 else
-  environmentID=""
   resourceGroupName=$1
+  if [ $# -eq 2 ]
+  then
+    environmentID=$2
+  else
+    environmentID=""
+  fi
 fi
 
 # Set the location for your services
@@ -17,6 +23,7 @@ location=australiaeast
 # Create a resource group if it does not exist
 if [ $(az group exists --name $resourceGroupName) = false ]
 then
+  echo "Create resource group $resourceGroupName ..."
   az group create \
   --name $resourceGroupName \
   --location $location
@@ -24,6 +31,7 @@ fi
 
 # create storage account
 export AZURE_STORAGE_ACCOUNT=superfundstorage$environmentID
+echo "Create storage $AZURE_STORAGE_ACCOUNT ..."
 az storage account create \
     --name $AZURE_STORAGE_ACCOUNT \
     --resource-group $resourceGroupName \
@@ -34,4 +42,5 @@ export AZURE_STORAGE_KEY=$(az storage account keys list --account-name $AZURE_ST
 
 # create Table Storage
 storageTableName=SuperfundTable$environmentID
+echo "Create table storage $storageTableName ..."
 az storage table create --name $storageTableName
--- SuperFundEnvironment/setup.sh
+++ SuperFundEnvironment/setup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+# Overide settings on the command line
+# ARG1 is resourceGroupName
+# ARG2 is keyVaultName
+if [ $# -eq 0 ]
+then
+  environmentID=$RANDOM
+  resourceGroupName=superfund-rg-$environmentID
+  keyVaultName=superfundkv$environmentID
+else
+  environmentID=""
+  resourceGroupName=$1
+  keyVaultName=$2
+fi
+
 # set execution context (if necessary)
 #az account set --subscription <replace with your subscription name or id>
 
-# Modify for your environment.
-keyVaultName=SuperFundKV
-
-# Set the resource group name and location for your server
-resourceGroupName=SuperfundLookupRG
+# Modify for your environment
 location=australiaeast
 
 # Create a resource group if it does not exist
 if [ $(az group exists --name $resourceGroupName) = false ]
 then
+  echo "Create resource group $resourceGroupName ..."
   az group create \
   --name $resourceGroupName \
   --location $location
@@ -22,9 +33,14 @@ fi
 az keyvault show --name $keyVaultName &> /dev/null
 if [ $? -ne 0 ]
 then
+  echo "Create key vault $keyVaultName ..."
   az keyvault create --name $keyVaultName --resource-group $resourceGroupName --location $location
 fi
 
 # Create the blob storage
-./setup-blobstorage.sh $resourceGroupName
-./setup-tablestorage.sh $resourceGroupName
+./setup-blobstorage.sh $resourceGroupName $environmentID
+./setup-tablestorage.sh $resourceGroupName $environmentID
+
+echo "environmentID: $environmentID"
+echo "resourceGroupName=: $resourceGroupName"
+echo "keyVaultName: $keyVaultName"
