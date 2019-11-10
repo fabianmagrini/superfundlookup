58357feb1fda7e998777718b619025df4a3e0709
Fabian Magrini <fabian.magrini@gmail.com>
2019-11-10 16:15:17 +1100

    added SuperFundTableStorage.Tests

---
+++ SuperFundTableStorage.Tests/SuperFundLookupRepositoryTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SuperFundTableStorage.Tests
+{
+    public class SuperFundLookupRepositoryTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
